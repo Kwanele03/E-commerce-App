cc18228a6ca609e7bbf5838179eadc402e735ea4
Kwanele <kwanelentshele03@gmail.com>
2025-02-15 04:18:31 +0200

    Image Repo added

---
+++ E-cormmece/src/main/java/com/enviro/practice/grad001/kwanelentshele/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package com.enviro.practice.grad001.kwanelentshele.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.enviro.practice.grad001.kwanelentshele.Model.Image;
+
+public interface ImageRepository extends JpaRepository<Image, Long>{
+
+}
+
