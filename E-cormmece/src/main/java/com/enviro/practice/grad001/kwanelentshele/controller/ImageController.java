ad024d3ecd658e6be1e7caf273c0035296bf638b
Kwanele <kwanelentshele03@gmail.com>
2025-02-15 14:20:14 +0200

    commit

--- E-cormmece/src/main/java/com/enviro/practice/grad001/kwanelentshele/controller/ImageController.java
+++ E-cormmece/src/main/java/com/enviro/practice/grad001/kwanelentshele/controller/ImageController.java
@@ -1,23 +1,16 @@
-Ipackage com.enviro.practice.grad001.kwanelentshele.controller;
+package com.enviro.practice.grad001.kwanelentshele.controller;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import com.enviro.practice.grad001.kwanelentshele.Model.Image;
@@ -25,8 +18,6 @@ import com.enviro.practice.grad001.kwanelentshele.dto.ImageDto;
 import com.enviro.practice.grad001.kwanelentshele.exceptions.ResourceNotFoundException;
 import com.enviro.practice.grad001.kwanelentshele.response.APIResponse;
 import com.enviro.practice.grad001.kwanelentshele.service.Image.IImageService;
-
-import jakarta.annotation.Resource;
 
 @RestController
 @RequestMapping("${api.prefix}/images")
@@ -58,61 +49,54 @@ private static final HttpStatus NOT_FOUND = HttpStatus.NOT_FOUND;
 		
   }
 	@GetMapping("/image/download/{imageId}")
-	public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException, IOException {
-	    Image image = imageServices.getImageById(imageId);
-
-	    // Assuming image.getImage() returns a Blob
-	    Blob imageBlob = image.getImage();
-	    byte[] imageBytes = imageBlob.getBytes(1, (int) imageBlob.length());
-	    ByteArrayResource resource = new ByteArrayResource(imageBytes);
-
-	    return ResponseEntity.ok()
-	            .contentType(MediaType.parseMediaType(image.getFileType()))
-	            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
-	            .body(resource);
+	public ResponseEntity<Resource> downloadImage( @PathVariable Long imageId) throws SQLException{
+		Image image = imageServices.getImageById(imageId);
+		
+		ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+		
+		return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+				
+		.header(HttpHeaders.CONTENT_DISPOSITION, "attchament; filename=\"" + image.getFileName() + "\"")
+		.body(resource);
 	}
 	
 	
 	@PutMapping ("/image/{}/update")
-	public ResponseEntity<APIResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file){
-		
-		Image image = imageServices.getImageById(imageId);
-	try {
-		if(image == null) {
-			imageServices.updateImage(file, imageId);
-		}
-		
-		return ResponseEntity.ok(new APIResponse( "Update Success", null));
-	}
-    
-	catch(ResourceNotFoundException exception) {
-		return ResponseEntity.status(NOT_FOUND).body(new APIResponse (exception.getMessage(),null));
-	}
-	
-	return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new APIResponse ("Update failed!", INTERNAL_SERVER_ERROR));
-	
+    public ResponseEntity<APIResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+        Image image = imageServices.getImageById(imageId);
+
+        if (image == null) {
+            return ResponseEntity.status(NOT_FOUND).body(new APIResponse("Image not found!", null));
+        }
+
+        try {
+            imageServices.updateImage(file, imageId);
+            return ResponseEntity.ok(new APIResponse("Update Success", null));
+        } catch (ResourceNotFoundException exception) {
+            return ResponseEntity.status(NOT_FOUND).body(new APIResponse(exception.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new APIResponse("Update failed!", null));
+        }
 	}
 	
 	
-	@DeleteMapping ("/image/{}/delete")
-	public ResponseEntity<APIResponse> deleteImage(@PathVariable Long imageId){
-		
-		Image image = imageServices.getImageById(imageId);
-	try {
-		if(image == null) {
-			imageServices.deleteImageById(imageId);
-		}
-		
-		return ResponseEntity.ok(new APIResponse( "Delete Success", null));
-	}
-    
-	catch(ResourceNotFoundException exception) {
-		return ResponseEntity.status(NOT_FOUND).body(new APIResponse (exception.getMessage(),null));
-	}
-	
-	return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new APIResponse ("Delete failed!", INTERNAL_SERVER_ERROR));
-	
-	}
+	@DeleteMapping ("/image/{imageId}/delete")
+    public ResponseEntity<APIResponse> deleteImage(@PathVariable Long imageId) {
+        Image image = imageServices.getImageById(imageId);
+
+        if (image == null) {
+            return ResponseEntity.status(NOT_FOUND).body(new APIResponse("Image not found!", null));
+        }
+
+        try {
+            imageServices.deleteImageById(imageId);
+            return ResponseEntity.ok(new APIResponse("Delete Success", null));
+        } catch (ResourceNotFoundException exception) {
+            return ResponseEntity.status(NOT_FOUND).body(new APIResponse(exception.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new APIResponse("Delete failed!", null));
+        }
+    }
 	
 	
 }
